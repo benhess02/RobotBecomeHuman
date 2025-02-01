e428dca426a9b964cec16d904f9fcb9ecaa1b8f7
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2025-01-31 23:39:51 -0800

    Added player dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1;
-
+    public float speed;
+    Vector2 movement;
     InputAction moveAction;
     Rigidbody2D rb;
+
+    public KeyCode dashKey;
+    public float maxDashTime = 10f;
+    public float currentDashTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentDashTime = maxDashTime;
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector2 movement = moveAction.ReadValue<Vector2>();
+    {   
+        if (Input.GetKeyDown(dashKey) && movement != Vector2.zero)
+            currentDashTime = 0.0f;
+        
+        if (currentDashTime < maxDashTime) {
+            currentDashTime += 0.1f;
+            speed = 30;
+        } else {
+            movement = moveAction.ReadValue<Vector2>();
+            speed = 8;
+        }
+
         rb.velocity = movement * speed;
+    }
+
+    private void Dash() {
+        
     }
 }
